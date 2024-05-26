87a7e11d403cd116f7ded62203cabf1d90c77108
Brian LeMaster <brian.lemaster@outlook.com>
2024-05-26 15:43:21 -0400

    added dto to entity mapper class in core project

---
+++ BookList.Core/Mappers/DTOToEntity.cs
@@ -0,0 +1,17 @@
+﻿using BookList.Core.DTO;
+using BookList.Core.Entities;
+
+namespace BookList.Core.Mappers;
+
+public static class DTOToEntity
+{
+    public static Author MapCreateUpdateAuthorDTOToAuthorEntity(CreateUpdateAuthorDTO dto)
+    {
+        return new()
+        {
+            Id = dto.Id,
+            Name = dto.Name,
+            IsFavorite = dto.IsFavorite,
+        };
+    }
+}
