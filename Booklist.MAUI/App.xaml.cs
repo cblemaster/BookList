0e37b0d01f5a2b490cb92d1e9d4f0c1bac381875
Brian LeMaster <brian.lemaster@outlook.com>
2024-05-25 20:21:04 -0400

    added MAUI project

---
+++ Booklist.MAUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Booklist.MAUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ Booklist.MAUI/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace Booklist.MAUI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
