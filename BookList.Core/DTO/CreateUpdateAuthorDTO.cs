d63f679439a11788fd450d40ef29aae6af96ea18
Brian LeMaster <brian.lemaster@outlook.com>
2024-05-26 15:43:00 -0400

    added createupdate author dto in core project

---
+++ BookList.Core/DTO/CreateUpdateAuthorDTO.cs
@@ -0,0 +1,19 @@
+﻿using BookList.Core.Validation;
+
+namespace BookList.Core.DTO;
+
+public class CreateUpdateAuthorDTO
+{
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public bool IsFavorite { get; set; }
+
+    public ValidationResult Validate()
+    {
+        if (!string.IsNullOrWhiteSpace(Name) && Name.Length > 0 && Name.Length <= 100)
+        {
+            return new() { IsValid = false, ErrorMessage = "Author name is required and must be between 1 and 100 characters." };
+        }
+        return new() { IsValid = true, ErrorMessage = string.Empty };
+    }
+}
