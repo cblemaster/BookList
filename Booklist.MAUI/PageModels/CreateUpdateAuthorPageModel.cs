b6f3be6e58b8d0a46984d982235837488820d717
Brian LeMaster <brian.lemaster@outlook.com>
2024-05-26 21:54:08 -0400

    completed createupdate author page model in MAUI project

--- Booklist.MAUI/PageModels/CreateUpdateAuthorPageModel.cs
+++ Booklist.MAUI/PageModels/CreateUpdateAuthorPageModel.cs
@@ -1,10 +1,35 @@
 ﻿using BookList.Core.DTO;
+using BookList.Core.Services;
+using BookList.Core.Validation;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Microsoft.Maui.Controls;
 
 namespace Booklist.MAUI.PageModels;
 
-public partial class CreateUpdateAuthorPageModel : ObservableObject
+public partial class CreateUpdateAuthorPageModel(IDataService dataService) : ObservableObject
 {
+    private readonly IDataService _dataService = dataService;
+
     [ObservableProperty]
     private CreateUpdateAuthorDTO _author = default!;
+
+    [RelayCommand]
+    private async Task SaveClicked()
+    {
+        ValidationResult validation = Author.Validate();
+        
+        if (!validation.IsValid)
+        {
+            await Shell.Current.DisplayAlert("Invalid Data!", validation.ErrorMessage, "OK");
+            return;
+        }
+
+        await _dataService.CreateAuthorAsync(Author);
+        await Shell.Current.Navigation.PopModalAsync();
+    }
+
+    [RelayCommand]
+    private async Task CancelClicked() =>
+        await Shell.Current.Navigation.PopModalAsync();
 }
